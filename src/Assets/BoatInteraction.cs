d71fba837216d3de990196070b54bf34cace6e14
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-11-29 15:59:17 +0100

    Boat interaction

---
+++ src/Assets/BoatInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoatInteraction : MonoBehaviour
+{
+    public string boatSceneName = "Boat";
+    public UI_InteractionTip controlsTip;
+    public string additionalText = "para entrar al barco";
+
+    private bool onRange = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onRange && Input.GetButtonUp("Interact")) {
+            SceneManager.LoadScene(boatSceneName);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        onRange = true;
+        controlsTip.setTip(additionalText);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        onRange = false;
+        controlsTip.unsetTip();
+    }
+}
---
+++ src/Assets/UI_InteractionTip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UI_InteractionTip : MonoBehaviour
+{
+    public string genericText = "Presiona E ";
+    private Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setTip(string text) {
+        _text.text = genericText + text;
+    }
+
+    public void unsetTip() {
+        _text.text = "";
+    }
+}
