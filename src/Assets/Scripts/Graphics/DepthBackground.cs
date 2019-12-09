9006ca5a607b18101d7a07ce704828ee74f39073
Unknown <lluiscan@ucm.es>
2019-12-09 13:34:50 +0100

    Nice depth water

---
+++ src/Assets/Scripts/Graphics/DepthBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DepthBackground : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+
+    public Transform player;
+    public float higherY, lowestY, maxAlpha, minAlpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentAlpha = Mathf.Clamp((((player.position.y - lowestY) / (higherY - lowestY)) * maxAlpha), minAlpha, maxAlpha);
+        Color color = sprite.color;
+        color.a = currentAlpha;
+        sprite.color = color;
+    }
+}
