fe7e5506e82ec97f6f993c7bdcb5cdfeabea5099
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-12-09 17:06:36 +0100

    Controls menu & exit button

---
+++ src/Assets/Scripts/UI/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour {
+    public bool onCancel = false;
+    public void ChangeScene() {
+        Application.Quit();
+    }
+
+    private void Update() {
+        if (onCancel && Input.GetButtonUp("Cancel")) Application.Quit();
+    }
+}
