cfe473e9e3b15c02adcfdec44cf76f143036d0b5
LluisCS <lluiscan@ucm.es>
2020-04-22 19:41:52 +0200

    EventTracker system

---
+++ src/Assets/Scripts/Tracking/Event.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventType { START, END, PHOTO, DEATH, UPGRADE, ENTERBOAT}
+
+public abstract class Event 
+{
+    protected EventType type;
+    protected double timeStamp;
+}
+
+public class StartEvent: Event
+{
+    string id;
+    public StartEvent(string ident)
+    {
+        type = EventType.START;
+        id = ident;
+        timeStamp = Time.time;
+    }
+}
+
+public class EndEvent : Event
+{
+    string id;
+    public EndEvent(string ident)
+    {
+        type = EventType.END;
+        id = ident;
+        timeStamp = Time.time;
+    }
+}
+
+public class PhotoEvent : Event
+{
+    Progress.Fish fishType;
+    public PhotoEvent(Progress.Fish fType)
+    {
+        fishType = fType;
+        type = EventType.PHOTO;
+        timeStamp = Time.time;
+    }
+}
+
+public class DeathEvent : Event
+{
+    int numGarbage, fishPhotos, percentUpgrade;
+    public DeathEvent(int nGarbage, int fPhotos, int pUpgrades) {
+        numGarbage = nGarbage;
+        fishPhotos = fPhotos;
+        percentUpgrade = pUpgrades;
+        type = EventType.DEATH;
+        timeStamp = Time.time;
+    }
+}
+
+public class BuyUpgradeEvent : Event
+{
+    int upgradeLevel, upgradeType;
+    public BuyUpgradeEvent(int upLevel, int upType)
+    {
+        upgradeLevel = upLevel;
+        upgradeType = upType;
+        type = EventType.UPGRADE;
+        timeStamp = Time.time;
+    }
+}
+
+public class EnterBoatEvent : Event
+{
+    int numGarbage, money;
+    public EnterBoatEvent(int nGarbage, int mon)
+    {
+        numGarbage = nGarbage;
+        money = mon;
+        type = EventType.ENTERBOAT;
+        timeStamp = Time.time;
+    }
+}
---
+++ src/Assets/Scripts/Tracking/EventTracker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//struct TrackerConfig { }
+
+public class EventTracker : MonoBehaviour
+{
+    private List<Event> events;
+    private string id;
+    public static EventTracker Instance = null;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+            Destroy(this);
+        id = System.DateTime.Now.ToString();
+        events = new List<Event>(0);
+    }
+
+    public void TrackEvent(Event ev)
+    {
+        events.Add(ev);
+    }
+
+    public void RegisterStartEvent() {
+        StartEvent ev = new StartEvent(id);
+        TrackEvent(ev);
+    }
+    public void RegisterEndEvent()
+    {
+        EndEvent ev = new EndEvent(id);
+        TrackEvent(ev);
+        //Clear?
+    }
+    public void RegisterDeathtEvent(int photos, int garbage, int lightUp, int speedUp, int oxygenUp, int bagUp, int maxUp)
+    {
+        int result = (int)(100 * (lightUp + speedUp + oxygenUp + bagUp) / (float)maxUp);
+        DeathEvent ev = new DeathEvent(photos, garbage, result);
+        TrackEvent(ev);
+    }
+
+    public void RegisterPhotoEvent(Progress.Fish fType)
+    {
+        PhotoEvent ev = new PhotoEvent(fType);
+        TrackEvent(ev);
+    }
+
+    public void RegisterUpgradeEvent(int upLevel, int upType)
+    {
+        BuyUpgradeEvent ev = new BuyUpgradeEvent(upLevel, upType);
+        TrackEvent(ev);
+    }
+
+    public void RegisterEnterEvent(int nGarbage, int mon)
+    {
+        EnterBoatEvent ev = new EnterBoatEvent(nGarbage, mon);
+        TrackEvent(ev);
+    }
+}
